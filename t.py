357120015befbae37ceddf6441c862bd9a176dc9
Alcides Fonseca <me@alcidesfonseca.com>
2018-02-27 13:31:21 +0000

    Added logs

--- t.py
+++ t.py
@@ -5,10 +5,11 @@ import z3
 
 if len(sys.argv) > 1:
     fname = sys.argv[1]
-    z3.Z3_open_log(fname + "_java.log")
+    z3.Z3_open_log(fname + "_python.log")
     s = z3.Solver()
     s.from_file(fname)
     print("python: " + str(s.check()))
+    z3.Z3_close_log()
 else:
     print(z3.get_full_version())
 
